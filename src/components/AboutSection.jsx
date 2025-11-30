cad80ad2c4cca8207d6dded1cdc966a2eadc7d4f
Akshit Sharma <akshitsharma07@Akshits-MacBook-Air.local>
2025-11-30 12:04:02 +0530

    added resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Akshit's_Resume.pdf" download={"Akshit's_Resume.pdf"}
+                href="/Akshit_resume.pdf" download={"Akshit_resume.pdf"}
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
